2dc1b5b9ecda4aead859333d40ecb868975cc0e8
Keerthi-Reddy-Baripireddy <144705049+Keerthi-Reddy-Baripireddy@users.noreply.github.com>
2023-11-24 11:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function functionName() {
-  // Get the function name from user input
-  var userInput = prompt("Enter the function name:");
-
-  // Display the entered function name using an alert statement
-  alert(userInput);
+function printFunctionName(name) {
+  alert(name);
 }
 
-// Example usage
-functionName(); // This will prompt the user to enter a function name and display it in an alert
+// Get user input
+var userInput = prompt();
+
+// Call the function with the user input
+printFunctionName(userInput);
